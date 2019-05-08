42d24fa8ff367458c1d8630d35a03714388f093d
taodtu <taodtu@gmail.com>
2019-05-08 19:34:43 +0100

    router

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ import * as serviceWorker from './serviceWorker';
 import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+if (module.hot) {
+ module.hot.accept()
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
