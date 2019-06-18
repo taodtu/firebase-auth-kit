88da5b0ae3053558acdd8f9ef9c68777f2d784aa
taodtu <taodtu@gmail.com>
2019-06-18 11:13:50 +0100

    init firebase db

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,5 +1,6 @@
 import app from 'firebase/app';
 import 'firebase/auth';
+import 'firebase/database'
 
 const config = {
  apiKey: "AIzaSyDLLczzFbLpoSV2uLIwqIT4LfOusz95mpI",
@@ -15,6 +16,7 @@ class Firebase {
   app.initializeApp(config);
 
   this.auth = app.auth();
+  this.db = app.database();
  }
  //***Auth API*** 
  doCreateUserWithEmailAndPassword = (email, password) =>
@@ -24,6 +26,9 @@ class Firebase {
  doSignOut = () => this.auth.signOut();
  doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
  doPasswordUpdate = password => this.auth.updatePassword(password);
+ // *** User API ***
+ user = uid => this.db.ref(`users/${uid}`);
+ users = () => this.db.ref('users');
 }
 
 export default Firebase;
