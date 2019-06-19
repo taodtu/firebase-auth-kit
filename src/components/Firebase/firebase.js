27a81ed623eb7ffdb0059bd391494cdf41fa2b55
codegaze <37536658+taodtu@users.noreply.github.com>
2019-06-19 16:34:31 +0100

    Update firebase.js

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -2,12 +2,12 @@ import app from 'firebase/app';
 import 'firebase/auth';
 
 const config = {
- apiKey: "AIzaSyDLLczzFbLpoSV2uLIwqIT4LfOusz95mpI",
- authDomain: "tao-firebase.firebaseapp.com",
- databaseURL: "https://tao-firebase.firebaseio.com",
- projectId: "tao-firebase",
- storageBucket: "tao-firebase.appspot.com",
- messagingSenderId: "101523633039",
+ apiKey: process.env.REACT_APP_API_KEY,
+authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+databaseURL: process.env.REACT_APP_DATABASE_URL,
+projectId: process.env.REACT_APP_PROJECT_ID,
+storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
 }
 
 class Firebase {
