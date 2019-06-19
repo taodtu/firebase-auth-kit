0ff5affa410b44d1abcf86ffee5ef170aca6d240
taodtu <taodtu@gmail.com>
2019-06-19 20:37:04 +0100

    add message app

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -15,8 +15,21 @@ class Firebase {
   constructor() {
     app.initializeApp(config);
 
+    /* Helper */
+
+    this.serverValue = app.database.ServerValue;
+    this.emailAuthProvider = app.auth.EmailAuthProvider;
+
+    /* Firebase APIs */
+
     this.auth = app.auth();
     this.db = app.database();
+
+    /* Social Sign In Method Provider */
+
+    this.googleProvider = new app.auth.GoogleAuthProvider();
+    this.facebookProvider = new app.auth.FacebookAuthProvider();
+    this.twitterProvider = new app.auth.TwitterAuthProvider();
   }
 
   // *** Auth API ***
@@ -27,9 +40,23 @@ class Firebase {
   doSignInWithEmailAndPassword = (email, password) =>
     this.auth.signInWithEmailAndPassword(email, password);
 
+  doSignInWithGoogle = () =>
+    this.auth.signInWithPopup(this.googleProvider);
+
+  doSignInWithFacebook = () =>
+    this.auth.signInWithPopup(this.facebookProvider);
+
+  doSignInWithTwitter = () =>
+    this.auth.signInWithPopup(this.twitterProvider);
+
   doSignOut = () => this.auth.signOut();
 
   doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+  doSendEmailVerification = () =>
+    this.auth.currentUser.sendEmailVerification({
+      url: process.env.REACT_APP_CONFIRMATION_EMAIL_REDIRECT,
+    });
 
   doPasswordUpdate = password =>
     this.auth.currentUser.updatePassword(password);
@@ -53,6 +80,8 @@ class Firebase {
             authUser = {
               uid: authUser.uid,
               email: authUser.email,
+              emailVerified: authUser.emailVerified,
+              providerData: authUser.providerData,
               ...dbUser,
             };
 
@@ -68,6 +97,12 @@ class Firebase {
   user = uid => this.db.ref(`users/${uid}`);
 
   users = () => this.db.ref('users');
+
+  // *** Message API ***
+
+  message = uid => this.db.ref(`messages/${uid}`);
+
+  messages = () => this.db.ref('messages');
 }
 
 export default Firebase;
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,10 +1,48 @@
-import React from 'react';
-import { withAuthorization } from '../Session';
-const HomePage = () => (
-  <div>
-    <h1>Home Page</h1>
-    <p>The Home Page is accessible by every signed in user.</p>
-  </div>
-);
+import React, { Component } from 'react';
+import { compose } from 'recompose';
+
+import { withAuthorization, withEmailVerification } from '../Session';
+import { withFirebase } from '../Firebase';
+import Messages from '../Messages';
+
+class HomePage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      users: null,
+    };
+  }
+
+  componentDidMount() {
+    this.props.firebase.users().on('value', snapshot => {
+      this.setState({
+        users: snapshot.val(),
+      });
+    });
+  }
+
+  componentWillUnmount() {
+    this.props.firebase.users().off();
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Home Page</h1>
+        <p>The Home Page is accessible by every signed in user.</p>
+
+        <Messages users={this.state.users} />
+      </div>
+    );
+  }
+}
+
 const condition = authUser => !!authUser;
-export default withAuthorization(condition)(HomePage);
+
+export default compose(
+  withFirebase,
+  withEmailVerification,
+  withAuthorization(condition),
+)(HomePage);
+
